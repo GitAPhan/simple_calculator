6a3f69c8b723dd3b92f4282bb36add86e541d30c
GitAPhan <itsandrewphan@gmail.com>
2022-01-25 20:51:47 -0700

    finished assignment, now onto the bonus

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from tokenize import Name
 import functions.addition as a
 import functions.subtraction as s
 import functions.multiplication as m
@@ -9,6 +10,8 @@ print('1: Addition')
 print('2: Subtraction')
 print('3: Multiplication')
 print('4: Division')
+print('5: Multiple numbers operation')
+print('* if option 5 is selected select from choices 1-4 afterwards')
 
 # user input
 try:
@@ -20,17 +23,31 @@ try:
     function_selection = int(function_selection)
     first_number = float(first_number)
     second_number = float(second_number)
-    
-except:
+except ValueError:
+    print('Please enter valid value')
+
+except Exception as e:
     print(e)
 
-if function_selection == 1:
-    answer = a.addition(first_number, second_number)
-elif function_selection == 2:
-    answer = s.subtraction(first_number, second_number)
-elif function_selection == 3:
-    answer = m.multiplication(first_number, second_number)
-elif function_selection == 4:
-    answer = d.division(first_number, second_number)
+try:
+    if function_selection == 1:
+        answer = a.addition(first_number, second_number)
+    elif function_selection == 2:
+        answer = s.subtraction(first_number, second_number)
+    elif function_selection == 3:
+        answer = m.multiplication(first_number, second_number)
+    elif function_selection == 4:
+        answer = d.division(first_number, second_number)
+except TypeError:
+    print('Please enter valid number to be calculated')
+except ZeroDivisionError:
+    print("You can't divide by zero")
+except Exception as e:
+    print(e)
 
-print('Your result: ', answer)
+try:
+    print('Your result: ', answer)
+except NameError:
+    print('We could not calculate your results, please enter valid numbers')
+except Exception as e:
+    print(e)
